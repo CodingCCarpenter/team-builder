62f37997d59f6477bec405f321835d8ace32a43d
CodingCCarpenter <christineassists@aol.com>
2019-10-09 00:14:27 -0500

    add Form.js file, components folder, and begin structure of App.js

---
+++ team-builder/src/Components/Form.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+        <div>
+            <p>This is filler text and will be replaced </p>
+            <p>This is just a little bit more filler text to give a second container.</p>
+        </div>
+    )
+}
